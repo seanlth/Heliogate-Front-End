d9196114c828abd1d6d3ce12c7a7011f9cee9495
Kevin <Kevin@KevinsLaptop>
2014-02-22 15:08:22 +0000

    initial commit

---
+++ VaadinTest3/src/com/example/vaadintest2/Vaadintest2Application.java
@@ -0,0 +1,30 @@
+package com.example.vaadintest2;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+
+
+public class Vaadintest2Application extends Application {
+	
+	private SplitPanel horizontalSplit = new SplitPanel(SplitPanel.ORIENTATION_HORIZONTAL);
+
+	DocEditor doc = new DocEditor();
+	
+	@Override
+	public void init() {
+		
+		buildMainLayout();
+		
+	}
+	
+	private void buildMainLayout() {
+		setMainWindow(new Window("Address Book Demo application"));
+		VerticalLayout layout = new VerticalLayout();
+		
+		layout.addComponent(doc);
+		getMainWindow().setContent(layout);
+	}
+
+}
+
