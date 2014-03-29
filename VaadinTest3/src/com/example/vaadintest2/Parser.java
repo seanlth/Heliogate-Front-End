ccd65a0d3ba5145c58a6af85c9ea6aeacd06cc9f
seanlth <seanlthcamera@gmail.com>
2014-03-29 15:57:42 +0000

    Fixed git trouble

--- VaadinTest3/src/com/example/vaadintest2/Parser.java
+++ VaadinTest3/src/com/example/vaadintest2/Parser.java
@@ -61,17 +61,4 @@ public class Parser {
 			e.printStackTrace();
 		}
 	}
-	private static Document convertStringToDocument(String xmlStr) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
-        DocumentBuilder builder;  
-        try 
-        {  
-            builder = factory.newDocumentBuilder();  
-            Document doc = builder.parse( new InputSource( new StringReader( xmlStr ) ) ); 
-            return doc;
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        } 
-        return null;
-    }
 }
